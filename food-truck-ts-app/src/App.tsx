c4f3a1aa2dfbb98f96115d79e174d5a5c8ba5319
aMotherFox <102123418+aMotherFox@users.noreply.github.com>
2022-10-10 17:57:40 -0500

    41 typescript app Adding Clickable Button (#3)
    * making a TS app
    
    * button is clickable
    
    * added button

--- food-truck-ts-app/src/App.tsx
+++ food-truck-ts-app/src/App.tsx
@@ -2,25 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App()  {
+
+const ourProject = "We are working on a food truck app"
+
+const handleSubmit = (e: React.ChangeEvent<any>) => {
+    console.log("I've been clicked")
+    e.preventDefault();
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {ourProject}
+
+        <div className="Body">
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <button className="button">
+              button
+            </button>
+          </form>
+        </div>
+
+        </div>
   );
-}
+};
 
 export default App;
