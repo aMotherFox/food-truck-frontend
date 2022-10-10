7958568227eff122a012e3c7aa85b54f42a117d3
aMotherFox <102123418+aMotherFox@users.noreply.github.com>
2022-10-10 17:31:38 -0500

    making a TS app

--- food-truck-ts-app/src/App.tsx
+++ food-truck-ts-app/src/App.tsx
@@ -2,25 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+const ourProject = "We are working on a food truck app"
+
+const handleSubmit = (e: React.ChangeEvent<any>) => {
+    console.log("I've been clicked")
+    e.preventDefault();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {ourProject}
+
+        <div className="Body">
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <button className="button"> {/*ONCLICK HERE*/}
+              Tweet
+            </button>
+          </form>
+        </div>
+
     </div>
   );
-}
+};
 
 export default App;
