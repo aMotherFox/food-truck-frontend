ff1fc09c180a5c6556435c8c2acb5701dec8c445
Daniel Ribeiro <danielrocha05lol@gmail.com>
2022-10-20 21:14:17 -0400

    password validation

--- src/SignUp.tsx
+++ src/SignUp.tsx
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+/*
+confirm Password need to match password.
 
+when the button is clicked, it will call a function that get the present values
+of password & confirmation password. if the values of password & confirmation password does
+NOT MATCH we will send a alert to the user screen to notifiy the user that
+the password is wrong. But if is right the sign up will be complete and give him an alert
+ stating that the user can log in by going on /login page
+
+*/
 const SignUp = () => {
+	const [values, setValues] = useState({
+		password: '',
+		confirmPassword: '',
+	});
+
+	const handleChange = (e: { target: { name: any; value: any } }) => {
+		const { name, value } = e.target;
+		setValues({
+			...values,
+			[name]: value,
+		});
+	};
 	const handleSubmit = () => {
+		if (values.password !== values.confirmPassword) {
+			alert('Password and Confirm Password does NOT match');
+		}
 		console.log(' submitted! ');
 	};
+
 	return (
 		<div>
 			<h1> This is the SignUp Page </h1>
-			<form className="form" id="form">
+			<form className="form">
 				<div className="form-body">
 					<div>
-						<label htmlFor="first-name">
+						<label htmlFor="firstname">
 							First Name
 							<input
-								id="first-name"
+								name="firstname"
 								type="text"
 								placeholder="First Name"
 								required
@@ -21,10 +46,10 @@ const SignUp = () => {
 						</label>
 					</div>
 					<div>
-						<label htmlFor="last-name">
+						<label htmlFor="lastname">
 							Last Name
 							<input
-								id="last-name"
+								name="last-name"
 								type="text"
 								placeholder="LastName"
 								required
@@ -34,19 +59,33 @@ const SignUp = () => {
 					<div>
 						<label htmlFor="email">
 							Email
-							<input type="email" placeholder="Email" required />
+							<input name="email" type="email" placeholder="Email" required />
 						</label>
 					</div>
 					<div>
 						<label htmlFor="password">
 							Password
-							<input type="password" placeholder="Password" required />
+							<input
+								name="password"
+								type="password"
+								placeholder="Password"
+								value={values.password}
+								onChange={handleChange}
+								required
+							/>
 						</label>
 					</div>
 					<div>
 						<label htmlFor="confirmation-password">
 							Confirm Password
-							<input type="password" placeholder="Confirm Password" required />
+							<input
+								name="confirmPassword"
+								type="password"
+								placeholder="Confirm Password"
+								value={values.confirmPassword}
+								onChange={handleChange}
+								required
+							/>
 						</label>
 					</div>
 				</div>
