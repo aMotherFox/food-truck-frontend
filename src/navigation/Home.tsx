7e0d1b3d1bb4b2544a4843b72bb972e5c6ae903f
vp <victoria.portela03@gmail.com>
2023-01-10 23:11:03 -0500

    pseudo coding and adding UI

--- src/navigation/Home.tsx
+++ src/navigation/Home.tsx
@@ -8,22 +8,28 @@ const Home = () => (
 		<h1>Zombie Foods </h1>
 		<h6>We swear its not people</h6>
 		<h3>Order here</h3>
-		<div>
-			<Tabs>
-				<TabList style={{ justifyContent: "center" }}>
-					<Tab>Appetizers</Tab>
-					<Tab>Entrees</Tab>
-				</TabList>
+		<div style={{display: "flex", justifyContent: "space-around" }}>
+			
+			<div style={{display: "flex", justifyContent: "center", width: "1000px", marginRight: "20px" }}>
+				<Tabs align="center">
+					<TabList>
+						<Tab>Appetizers</Tab>
+						<Tab>Entrees</Tab>
+					</TabList>
 
-				<TabPanels>
-					<TabPanel>
-						<AppetizerTab />
-					</TabPanel>
-					<TabPanel>
-						<EntreeTab />
-					</TabPanel>
-				</TabPanels>
-			</Tabs>
+					<TabPanels>
+						<TabPanel>
+							<AppetizerTab />
+						</TabPanel>
+						<TabPanel>
+							<EntreeTab />
+						</TabPanel>
+					</TabPanels>
+				</Tabs>
+			</div>
+			<div style={{backgroundColor: "grey", display: "flex", justifyContent: "center", width: "250px", height: "350px"}}>
+				<h1 > this is where your cart lives</h1>
+			</div>
 		</div>
 	</div>
 );
--- src/orders/AppetizerTab.tsx
+++ src/orders/AppetizerTab.tsx
@@ -1,13 +1,53 @@
+import {
+	Button,
+	Card,
+	CardBody,
+	CardFooter,
+	CardHeader,
+	Heading,
+	SimpleGrid,
+} from "@chakra-ui/react";
 import React from "react";
 // import axios from "axios";
 
 const AppetizerTab = () => {
-	const test = "test";
+	const test = "add apps to your order";
+
+	// story todos MACRO
+	// display appetizers in cards
+	// display entrees in cards
+	// make front end only/hardcoded orders where the apps/entrees get added to the cart when selected
+
+	// user selects an appetizer to add to order
+	// we already have app ids and info on the front end since they're being displayed, right?
+	// selected app is added to an order array of apps and entrees which includes their ids and stuff
+	// this order array is displayed on the front end in the cart? 
+	// selected app is fetched and displayed in the cart box (is this too resource intensive? should I do this without calling the api yet? or is there no other good way?)
+	// when a user is ready to checkout they click the checkout box
+	// this calls the create order api
+	// sending the request body to the endpoint
+	// when this is successful
+	// the user is pushed to a confirmation page
 
 	return (
 		<>
-			<h1>Appetizers</h1>
-			<h2>${test}</h2>
+			<h2>{test}</h2>
+			<SimpleGrid
+				spacing={4}
+				templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+			>
+				<Card align="center">
+					<CardHeader>
+						<Heading size="md"> Appetizer 1</Heading>
+					</CardHeader>
+					<CardBody>
+						<h1>View a summary of all your customers over the last month.</h1>
+					</CardBody>
+					<CardFooter>
+						<Button>Add to order</Button>
+					</CardFooter>
+				</Card>
+			</SimpleGrid>
 		</>
 	);
 };
